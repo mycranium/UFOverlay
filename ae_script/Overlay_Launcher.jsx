b59c9e43280fd5dd55d8ad9495962fd7911ccd56
Mike Rainey <7647165+mycranium@users.noreply.github.com>
2024-03-21 14:56:10 -0400

    v0.3 adds namespacing, error handling and other housekeeping tasks.

--- ae_script/Overlay_Launcher.jsx
+++ ae_script/Overlay_Launcher.jsx
@@ -1,3 +1,5 @@
 #include 'json2.js';
-#include 'Overlay_Maker_v0.3.jsx'
-var ovlSave = makeOverlays();
+#include 'Overlay_Maker_v0.3.jsx';
+//clearOutput();
+var overlayGlobals = overlayGlobals || {};
+var make = makeOverlays();
